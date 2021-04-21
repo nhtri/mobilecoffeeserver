48609c3dcbe482895f049bd4adc1b4e0d2fc979a
tringuyen <tri.nh@hotmail.com>
2021-04-21 23:04:59 +0700

    updated

--- main.js
+++ main.js
@@ -307,7 +307,7 @@ app.post('/device/', function (req, res) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 
-    pool.query('INSERT INTO mobilephone VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12,$13,$14,$15,$16)', postData, function (error, results, fields) {
+    pool.query('INSERT INTO mobilephone VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, DEFAULT,$12,$13,$14,$15)', postData, function (error, results, fields) {
         if (error) throw error;
         res.end(JSON.stringify(results.rows));
     });
