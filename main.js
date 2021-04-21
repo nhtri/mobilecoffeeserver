a5cdd1bab76cd4e41d2d385a139aa35a5e394511
tringuyen <tri.nh@hotmail.com>
2021-04-21 15:50:33 +0700

    updated

--- main.js
+++ main.js
@@ -300,4 +300,29 @@ app.get('/detail/', function (req, res) {
     });
 });
 
+app.post('/device/', function (req, res) {
+    var postData = req.body;
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('INSERT INTO mobilephone VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12,$13,$14,$15,$16)', postData, function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
+app.delete('/device/:id', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+    pool.query('DELETE FROM mobilephone WHERE id = $1', [req.params.id], function (error, results) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
 
