283ee7e2e70bbaf4524ef6839bdbf402b85995fb
Tri Nguyen <tri.nguyen1@tyme.com>
2021-05-07 21:53:26 +0700

    up

--- main.js
+++ main.js
@@ -253,4 +253,42 @@ app.get('/airpod/', function (req, res) {
     });
 });
 
+app.get('/simdata/', function (req, res) {
+    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+    console.log(req);
+	pool.query("select id, category, name, image1, price, active, summary from mobilephone where category = 'sim_data_wifi_new' ORDER BY summary", function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
+app.get('/dienthoaicu/', function (req, res) {
+    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+    console.log(req);
+	pool.query("select id, category, name, image1, price, active, summary from mobilephone where category = 'dienthoaicu' ORDER BY summary", function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
+
+app.get('/iphone/', function (req, res) {
+    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
+    console.log(req);
+	pool.query("select id, category, name, image1, price, active, summary from mobilephone where category = 'iphone_new' ORDER BY summary", function (error, results, fields) {
+        if (error) throw error;
+        res.end(JSON.stringify(results.rows));
+    });
+});
 
